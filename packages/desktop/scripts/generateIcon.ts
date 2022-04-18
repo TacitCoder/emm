2c46297e45a663dc090f38195edea34ed8446b7a
Tacit Coder <tacitcoder@gmail.com>
2022-04-18 14:56:41 +0800

    chore(script): fix scripts for tooling

--- packages/desktop/scripts/generateIcon.ts
+++ packages/desktop/scripts/generateIcon.ts
@@ -1,3 +1,5 @@
 import Sharp from 'sharp'
+import { run } from '../../../scripts/utils'
 
-Sharp('../website/docs/.vuepress/public/favicon.svg', { density: 1024 }).resize(1024).toFile('node_modules/favicon.png')
+Sharp('../website/docs/.vuepress/public/favicon.svg', { density: 4096 }).resize(1024).toFile('node_modules/favicon.png')
+run('tauricon', ['node_modules/favicon.png'])
--- scripts/preinstall.ts
+++ scripts/preinstall.ts
@@ -1,3 +1,7 @@
+import { writeFile } from 'fs/promises'
+import { existsSync } from 'fs'
+import fetch from 'node-fetch'
+import { path7za } from '7zip-bin'
 import { run } from './utils'
 
 const dependencies = [
@@ -20,8 +24,18 @@ const dependencies = [
 ]
 
 async function main() {
-  for (let i = 0; i < dependencies.length; i++)
-    run('git', ['clone', dependencies[i].url, dependencies[i].path])
+  for (let i = 0; i < dependencies.length; i++) {
+    if (!existsSync(dependencies[i].path))
+      run('git', ['clone', dependencies[i].url, dependencies[i].path])
+  }
+  const url = 'https://mirrors.tuna.tsinghua.edu.cn/github-release/be5invis/Sarasa-Gothic/LatestRelease/'
+  let res = await fetch(url)
+  const html = await res.text()
+  const fontFileName = html.match(/sarasa-gothic-ttf-\d+\.\d+\.\d+\.7z/)[0]
+  const fontUrl = url + fontFileName
+  res = await fetch(fontUrl)
+  await writeFile('dependencies/sarasa-gothic-ttf.7z', res.body)
+  await run(path7za, ['e', 'dependencies/sarasa-gothic-ttf.7z', '-i!sarasa-mono-sc-regular.ttf', '-odependencies/'])
 }
 
 main().catch((err) => { console.error(err) })
